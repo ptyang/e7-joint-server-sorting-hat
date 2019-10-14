bfc324c16ae0410d16011696d68d63bfac07e78f
ptyang <py7133@hotmail.com>
2019-10-14 16:56:40 -0400

    Updates

---
+++ precache-manifest.e65cfab79edb5e8b1ce46435ffd73308.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56f708e753c5c9030352c75f9fe2c393",
+    "url": "/e7-joint-server-sorting-hat/index.html"
+  },
+  {
+    "revision": "38e4847e8a9ca8d0162c",
+    "url": "/e7-joint-server-sorting-hat/static/css/main.04ffee74.chunk.css"
+  },
+  {
+    "revision": "90c118a92eceb90d3e89",
+    "url": "/e7-joint-server-sorting-hat/static/js/2.3e836a62.chunk.js"
+  },
+  {
+    "revision": "38e4847e8a9ca8d0162c",
+    "url": "/e7-joint-server-sorting-hat/static/js/main.8bf81331.chunk.js"
+  },
+  {
+    "revision": "34683b73bd4897d75985",
+    "url": "/e7-joint-server-sorting-hat/static/js/runtime-main.ede8ca46.js"
+  }
+]);
