25e081f7fecc6c70d14861ddafd096de87ffd7e0
ptyang <py7133@hotmail.com>
2019-10-14 16:42:36 -0400

    Attempt with docs folder

---
+++ sorting-hat/docs/precache-manifest.6d2a32e51d488aebf03dbec68628d41d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51a1b82af706be37b288b36cb115eb86",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a263dcc938224f8a0314",
+    "url": "/static/css/main.04ffee74.chunk.css"
+  },
+  {
+    "revision": "90c118a92eceb90d3e89",
+    "url": "/static/js/2.3e836a62.chunk.js"
+  },
+  {
+    "revision": "a263dcc938224f8a0314",
+    "url": "/static/js/main.3ce914ff.chunk.js"
+  },
+  {
+    "revision": "371dc8d269bdd04423ba",
+    "url": "/static/js/runtime-main.fa3a1de2.js"
+  }
+]);
